18de8f564d5d6086d3cc44fc0303a4aaf8173079
mdbenum <mari.dahl.benum@tellu.no>
2026-05-10 18:12:17 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -347,7 +347,7 @@ const runDeliverySequence = () => {
 					const preTranslateX = Number.parseFloat(invitationContent.style.getPropertyValue('--invitation-reveal-translate-x')) || 0;
 					const preTranslateY = Number.parseFloat(invitationContent.style.getPropertyValue('--invitation-reveal-translate-y')) || 0;
 					invitationContent.style.transform = `translate(${preTranslateX}px, ${preTranslateY}px) scale(${preScaleX}, ${preScaleY})`;
-					invitationContent.style.opacity = '1';
+					invitationContent.style.opacity = '0';
 				}
 				deliverySequence.classList.add('is-paper-expanding');
 				startDeliveryBackgroundReveal();
